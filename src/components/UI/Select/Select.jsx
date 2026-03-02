652f3bba9be1b1976b5b6fc8c97b04ed7ae977bb
mikhailpopkov <m.popkov.02@mail.ru>
2026-03-03 00:14:17 +0300

    Небольшой фикс к предыдущему

--- src/components/UI/Select/Select.jsx
+++ src/components/UI/Select/Select.jsx
@@ -1,9 +1,10 @@
 import cl from './Select.module.scss';
 
 function Select({defaulValue, value, options, onChangeSort}) {
+
     return (
-        <select className={cl.select} name='newsSort' onChange={e => onChangeSort(e.target.value)}>
-            <option disabled value={value}>{defaulValue}</option>
+        <select className={cl.select} name='newsSort' onChange={e => onChangeSort(e.target.value)} value={value}>
+            <option disabled value=''>{defaulValue}</option>
             {
                 options.map((option => 
                     <option key={option.value} value={option.value}>{option.name}</option>
